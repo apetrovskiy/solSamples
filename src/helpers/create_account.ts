0d76be43adab35fbf53b3fee037f27ab028c89fd
Alexander Petrovskiy <apetrovskiy2@hotmail.com>
2021-08-15 23:36:52 +0300

    code clean-up

--- src/helpers/create_account.ts
+++ src/helpers/create_account.ts
@@ -1,28 +1,10 @@
-// require('./connect_probe.ts');
-
-
-
-// console.log(web3);
-// console.log(url_evm);
-
-
-// console.log(typeof result);
-
 export class AccountCreator {
     create(): object {
         require('dotenv').config();
-
-        // const randomstring = require('randomstring');
-
         let Web3 = require('web3');
         let url_evm = process.env.RPC_URL;
         let web3 = new Web3(url_evm);
-
-        // const randomString = randomstring.generate(32);
         let result = web3.eth.accounts.create();
-        //randomString);
-        // console.log(result);
-
         return result;
     }
 }
